b893ec28dd79af57e7491e218c522ecf7196f96f
wuhongyi <862474311@qq.com>
2022-04-17 18:26:26 +0800

    实现effect的stop功能
    Signed-off-by: wuhongyi5 <wuhongyi5@jd.com>

---
+++ src/shared/index.ts
@@ -0,0 +1,9 @@
+/*
+ * @Author: wuhongyi5
+ * @Date: 2022-04-17 18:21:17
+ * @LastEditors: wuhongyi5
+ * @LastEditTime: 2022-04-17 18:21:17
+ * @FilePath: /why-mini-vue3/src/shared/index.ts
+ * @description: 
+ */
+export const extend = Object.assign
